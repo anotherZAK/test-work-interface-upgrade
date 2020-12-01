e136e787aac6dace32fdebb41be941edede9be71
Pavel <pasha.krupko@yandex.ru>
2020-12-01 16:07:29 +0300

    Подготовил блок с дополнительной навигацией по главным пунктам программы страницы

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,14 @@
 'use strict'
 
+const MAX_Y_OFFSET = 500;
 const Tags = {
   a: `A`,
   h2: `H2`
 };
 const treeBlockList = document.querySelectorAll(`.tree__block`);
+const treeNav = document.querySelector(`.tree-navigation`);
+
+
 
 /**
  * переключает класс для переданного селектора
@@ -12,7 +16,7 @@ const treeBlockList = document.querySelectorAll(`.tree__block`);
  * @return {Object} - функция, переключающая класс
  */
 const treeHeadHandler = function (container) {
-  return (evt) => {
+  return function (evt) {
     evt.preventDefault();
     try {
       if (Object.values(Tags).includes(evt.target.tagName)) {
@@ -23,6 +27,19 @@ const treeHeadHandler = function (container) {
   };
 };
 
+/**
+ * переключает класс для меню дополнительной навигации
+ */
+const treeNavigationHandler = function() {
+  if (pageYOffset > MAX_Y_OFFSET) {
+    console.log(pageYOffset);
+    treeNav.classList.add(`tree-navigation--visible`);
+  } else {
+    treeNav.classList.remove(`tree-navigation--visible`);
+  }
+}
+
 for (const treeBlock of treeBlockList) {
   treeBlock.addEventListener(`click`, treeHeadHandler(treeBlock));
 }
+window.addEventListener(`scroll`, treeNavigationHandler);
