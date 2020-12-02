245e924b6467ffbacc9574abe87736b61a1745b7
Pavel <pasha.krupko@yandex.ru>
2020-12-02 14:11:03 +0300

    Выполнил сборку проекта с помощью gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,32 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const jsmin = require("gulp-jsmin");
+const rename = require("gulp-rename");
+const del = require("del");
 
 const clean = () => {
-  return del("build");
+  return del(["docs"]);
 }
+
+exports.clean = clean;
+
+const copy = () => {
+  return gulp.src([
+      "source/*.html",
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("docs"));
+};
+
+exports.copy = copy;
 
 // Styles
 
 const styles = () => {
-  return gulp.src("docs/less/style.less")
+  return gulp.src("source/less/style.less")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(less())
@@ -21,11 +38,41 @@ const styles = () => {
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("docs/css"))
+    .pipe(gulp.dest("source/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+const stylesMin = () => {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename({
+      suffix: "-min"
+    }))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("docs/css"))
+    .pipe(sync.stream());
+}
+
+exports.stylesMin = stylesMin;
+
+const jsStyles = () => {
+  return gulp.src("source/js/*.js")
+    .pipe(jsmin())
+    .pipe(rename({
+      suffix: "-min"
+    }))
+    .pipe(gulp.dest("docs/js"));
+}
+
+exports.jsStyles = jsStyles;
 
 // Server
 
@@ -46,7 +93,7 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("docs/less/**/*.less", gulp.series("styles"));
+  gulp.watch("docs/less/**/*.less", gulp.series("stylesMin"));
   gulp.watch("docs/*.html").on("change", sync.reload);
 }
 
@@ -57,8 +104,12 @@ exports.default = gulp.series(
 
 const build = (done) => {
   gulp.series(
-    clean
+    clean, copy, stylesMin, jsStyles
   )(done);
 };
 
 exports.build = build;
+
+exports.start = gulp.series(
+  build, server, watcher
+);
